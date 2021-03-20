b4b6004317671a3ba0f71bd488101c1931f59cc6
Eastwood9 <buruntaroff@gmail.com>
2021-03-20 03:28:06 +0300

    lesson 12 95%

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,16 @@
 $(function() {
+
+  $('.top-slider__inner').slick({
+    arrows: false,
+    dots: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 6000
+  })
+
+  $(".week__item").hover(function () {
+    $(this).toggleClass('week__item--active');
+  });
+
+  var mixer = mixitup('.week__content');
 });
